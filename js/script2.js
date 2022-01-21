510afb8e4f96422059546d8c2391e847facad5c5
Jake Wylie <jake.d.wylie@gmail.com>
2022-01-20 18:43:04 -0700

    Added slideshow, changed fonts, changed the words on the sound bath page

---
+++ js/script2.js
@@ -0,0 +1,14 @@
+// (function (){
+// var headerHtml = "snippets/headerSnippet.html";
+
+// // document.getElementById("header").innerHTML = headerHtml;
+// // })();
+
+// $ajaxUtils.sendGetRequest(
+//   headerHtml,
+//   function (responseText) {
+//     document.querySelector("#header")
+//       .innerHTML = responseText;
+//   },
+//   false);
+// });
